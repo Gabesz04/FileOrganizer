5c35a05ea70d1f9754333cb0859d14789c7345d0
Gábor <gombosgabor2004@gmail.com>
2024-02-09 15:23:27 +0100

    hasdir, helping funcs

--- main.go
+++ main.go
@@ -15,7 +15,41 @@ func main() {
 	}
 
 	listElements(files)
+	makeDir(files)
+}
 
+func makeDir(f []fs.DirEntry) {
+	stringEnd := ""
+	for _, file := range f {
+		stringEnd = file.Name()[len(file.Name())-3:]
+		if stringEnd == "pdf" {
+			HasDir(file, f)
+		}
+	}
+}
+
+func HasDir(file fs.DirEntry, files []fs.DirEntry) {
+	for _, f := range files {
+		if GetXFirstElements(8, f) == GetXFirstElements(8, file) && f.IsDir() == false {
+			fmt.Println("OTTVAGYUNK")
+		}
+	}
+}
+
+func GetXFirstElements(n int, f fs.DirEntry) string {
+	if len(f.Name()) > n {
+		return f.Name()[0:n]
+	} else {
+		return ""
+	}
+}
+
+func GetXLastElements(n int, f fs.DirEntry) string {
+	if len(f.Name()) > n {
+		return f.Name()[len(f.Name())-n:]
+	} else {
+		return ""
+	}
 }
 
 func listElements(f []fs.DirEntry) {
